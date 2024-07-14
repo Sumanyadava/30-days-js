0658b3709ea2a2b7518ef7a70f2faec36f22367e
Suman <ansum2411@gmail.com>
2024-07-14 19:05:03 +0530

    day1

---
+++ day1.js
@@ -0,0 +1,29 @@
+var num = 3046;
+let fname = "suman"
+console.log(num , fname);
+
+// act 2 
+const flag = true;
+console.log(flag);
+
+// act 3
+const person = {
+    finame : "string",
+    lname : "string",
+    age : 23,
+    employed : false
+}
+
+console.log(typeof num , typeof fname , typeof flag ,typeof person)
+// act 4
+let a = 10;
+console.log(a)
+a= 20
+console.log(a)
+// act 5
+
+// flag = false
+// assigment to constant variable
+
+
+console.log(typeof num, num , typeof fname,fname , typeof flag ,flag,typeof person,person)
